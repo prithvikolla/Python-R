2f92aa196f250e71a30d0cf49995efb5c7a0e721
prithvi <prithvikolla8@gmail.com>
2020-08-06 19:03:52 -0500

    project start

---
+++ R/BigMart_Sales_Prediction/BigMarket_Sales.R
@@ -0,0 +1,9 @@
+library(data.table) #used for reading and manupulation of data
+library(dplyr) #data manupulation and joining library
+library(caret) #modeling
+library(corrplot) #correlation plots
+library(xgboost) # creating xgboost model
+library(cowplot) # combining multiple plots
+library(ggplot2) #plotting
+
+
