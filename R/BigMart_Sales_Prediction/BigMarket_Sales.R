153c418349ef304227ea470cc41139c1c2baacf8
prithvi <prithvikolla8@gmail.com>
2020-08-07 10:45:00 -0500

    Update
    Some work on Code

--- R/BigMart_Sales_Prediction/BigMarket_Sales.R
+++ R/BigMart_Sales_Prediction/BigMarket_Sales.R
@@ -5,5 +5,12 @@ library(corrplot) #correlation plots
 library(xgboost) # creating xgboost model
 library(cowplot) # combining multiple plots
 library(ggplot2) #plotting
-
-
+train = fread("/Users/prithvinagkolla/Documents/GitHub/Python-R/R/BigMart_Sales_Prediction/train_v9rqX0R.csv")
+test = fread("/Users/prithvinagkolla/Documents/GitHub/Python-R/R/BigMart_Sales_Prediction/test_AbJTz2l.csv")
+dim(train) #finding dimensions for better understanding
+dim(test)
+names(train)
+names(test)
+test[,Item_Outlet_Sales:= NA]
+combi = rbind(train,test) #rbind is used to combine data
+dim(combi)
