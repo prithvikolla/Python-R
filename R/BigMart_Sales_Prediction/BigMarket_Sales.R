578d911c9f03417fc63822a50155976675e3865f
prithvi <prithvikolla8@gmail.com>
2020-08-17 22:59:56 -0500

    update

--- R/BigMart_Sales_Prediction/BigMarket_Sales.R
+++ R/BigMart_Sales_Prediction/BigMarket_Sales.R
@@ -27,3 +27,12 @@ combi$Item_Fat_Content[combi$Item_Fat_Content == "low fat"] = "Low Fat"
 combi$Item_Fat_Content[combi$Item_Fat_Content == "reg"] = "Regular"
 ggplot(combi %>% group_by(Item_Fat_Content) %>% summarise(Count = n())) + 
   geom_bar(aes(Item_Fat_Content,Count), stat = "Identity", fill = "blue")
+p4 = ggplot(combi %>% group_by(Item_Type) %>% summarise(Count = n())) + geom_bar(aes(Item_Type,Count), stat = "identity", fill = "blue")+ geom_label(aes(Item_Type,Count,label = Count), vjust = 0.5)+ theme(axis.test.x = element_text(angle = 45,hjust = 1))+ ggtitle("Item_Type")
+p5 = ggplot(combi %>% group_by(Outlet_Identifier) %>% summarise(Count = n())) + geom_bar(aes(Outlet_Identifier,Count), stat = "identity", fill = "blue")+ geom_label(aes(Outlet_Identifier,Count,label = Count), vjust = 0.5)+ theme(axis.test.x = element_text(angle = 45,hjust = 1))
+
+p6 = ggplot(combi %>% group_by(Outlet_Size) %>% summarise(Count = n())) + geom_bar(aes(Outlet_Size,Count), stat = "identity", fill = "blue")+ geom_label(aes(Outlet_Size,Count,label = Count), vjust = 0.5)+ theme(axis.test.x = element_text(angle = 45,hjust = 1))
+
+secondrow = plot_grid(p5,p6,nrow = 1) 
+
+plot_grid(p4,secondrow,ncol = 1)
+
